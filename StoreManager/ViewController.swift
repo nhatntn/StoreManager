862c8eb838e5d00e5382ce5b72bd18d4009ccca2
nhatnt <nhatnt@vng.com.vn>
2020-05-09 13:48:43 +0700

    Initial Commit

---
+++ StoreManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreManager
+//
+//  Created by nhatnt on 5/9/20.
+//  Copyright © 2020 nhatnt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
